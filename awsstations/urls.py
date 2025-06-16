a76c07bba42aa7366ccb1ba0cb769a7cb96629ee
Muhammed Rashid <100080362+muhammedrashidx@users.noreply.github.com>
2025-06-16 23:30:22 +0530

    Update urls.py

--- awsstations/urls.py
+++ awsstations/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('stations/<int:station_id>/', views.StationDetailView.as_view()),
     path('stations/<int:station_id>/rawdata/', views.StationRawDataView.as_view()),
     path('train/', views.TrainStationListView.as_view()),
-    path('daily-station-data/<int:station_id>/', views.DailyStationDataListView.as_view())
+    path('daily-station-data/<int:station_id>/', views.DailyStationDataListView.as_view()),
+    path('quarterly-aws-data/<int:station_id>/', QuarterlyAWSDataCreateView.as_view()),
+
 ]
